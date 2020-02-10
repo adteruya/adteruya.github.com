338f911807f40a8986cfdd214ac72891014ae954
Ana Teruya <adteruya@ucdavis.edu>
2020-02-09 19:27:42 -0800

    issues loading up js file

---
+++ des157-wi20/studio1/script.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+    console.log("hullo this is a test");
+}());
+
+/**document.getElementById("plant-form").addEventListener("submit", function(event)    {
+    // Prevent the default behavior from occuring:
+    event.preventDefault();
+
+    // Find style choice:
+    var styleList = document.querySelectorAll('input[name="type"]');
+    var userStyle;
+    for (let i = 0; i < styleList.length; i++)  {
+        if (styleList[i].checked)   {
+            userStyle = styleList[i].value;
+            break;
+        }
+    }
+});**/
