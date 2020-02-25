55ea2c30988b0cb3c0678fa8574f0aec98c5deae
Ana Teruya <adteruya@ucdavis.edu>
2020-02-25 05:54:44 -0800

    finished game

--- des157-wi20/studio3/script.js
+++ des157-wi20/studio3/script.js
@@ -47,33 +47,44 @@
         document.getElementById('player-info').style.height = 'auto';
         document.getElementById('player-info').style.margin = '0 auto';
         document.getElementById('player-info').style.marginTop = '20px';
-        document.getElementById('curr-player').innerHTML = `<h4>${match.players[match.curr_player]}'s Turn</h4>`;
-        document.getElementById('scores').innerHTML = `<h4>Player 1: ${match.score[0]} | Player 2: ${match.score[1]}</h4>`;
-
-        selectCard();
+        if (match.flipped == match.game_end)    {
+            if (match.score[0] > match.score[1])    {
+                document.getElementById('curr-player').innerHTML = `<h2>${match.players[0]} WON!</h2>`;
+                document.getElementById('curr-player').style.color = '#92140C';
+            } else  {
+                document.getElementById('curr-player').innerHTML = `<h2>${match.players[1]} WON!</h2>`;
+                document.getElementById('curr-player').style.color = '#92140C';
+            }
+        } else  {
+            document.getElementById('curr-player').innerHTML = `<h4>${match.players[match.curr_player]}'s Turn</h4>`;
+            document.getElementById('scores').innerHTML = `<h4>Player 1: ${match.score[0]} | Player 2: ${match.score[1]}</h4>`;
+        }
     }
 
-    function selectCard(){
-        // Pick Card:
-        document.getElementById('card1').addEventListener('click', function()   {
-            chooseCard('card1');
-        });
-        document.getElementById('card2').addEventListener('click', function()   {
-            chooseCard('card2');
-        });
-        document.getElementById('card3').addEventListener('click', function()   {
-            chooseCard('card3');
-        });
-        document.getElementById('card4').addEventListener('click', function()   {
-            chooseCard('card4');
-        });
-        document.getElementById('card5').addEventListener('click', function()   {
-            chooseCard('card5');
-        });
-        document.getElementById('card6').addEventListener('click', function()   {
-            chooseCard('card6');
-        });
-    }
+    // Pick Card:
+    document.getElementById('card1').addEventListener('click', function()   {
+        chooseCard('card1');
+    });
+
+    document.getElementById('card2').addEventListener('click', function()   {
+        chooseCard('card2');
+    });
+
+    document.getElementById('card3').addEventListener('click', function()   {
+        chooseCard('card3');
+    });
+
+    document.getElementById('card4').addEventListener('click', function()   {
+        chooseCard('card4');
+    });
+
+    document.getElementById('card5').addEventListener('click', function()   {
+        chooseCard('card5');
+    });
+
+    document.getElementById('card6').addEventListener('click', function()   {
+        chooseCard('card6');
+    });
 
     function chooseCard(cardPicked)   {
         console.log(`beginning: ${match.cards_picked}`);
@@ -118,7 +129,7 @@
     function checkIfMatch() {
         let selection = document.getElementsByClassName('flipped');
         if (selection.length < 2)   {
-            selectCard();
+            // Do Nothing
         } else  {
             if ((selection[0].id === 'card1') && (selection[selection.length - 1].id === 'card3'))  {
                 document.getElementById('circles1').setAttribute('class', 'match');
@@ -129,6 +140,8 @@
                 match.cards_picked = 0;
                 match.score[match.curr_player] += 1;
                 match.flipped += 2;
+
+                document.getElementById('scores').innerHTML = '<h4>You made a match!</h4>';
             }
             else if ((selection[0].id === 'card2') && (selection[selection.length - 1].id === 'card6'))  {
                 document.getElementById('squares1').setAttribute('class', 'match');
@@ -139,6 +152,8 @@
                 match.cards_picked = 0;
                 match.score[match.curr_player] += 1;
                 match.flipped += 2;
+
+                document.getElementById('scores').innerHTML = '<h4>You made a match!</h4>';
             }
             else if ((selection[0].id === 'card4') && (selection[selection.length - 1].id === 'card5'))  {
                 document.getElementById('triangles1').setAttribute('class', 'match');
@@ -149,6 +164,8 @@
                 match.cards_picked = 0;
                 match.score[match.curr_player] += 1;
                 match.flipped += 2;
+
+                document.getElementById('scores').innerHTML = '<h4>You made a match!</h4>';
             } 
             else  {
                 console.log(selection.length);
@@ -156,11 +173,16 @@
                 selection[selection.length - 1].setAttribute('class', 'unflipped');
 
                 let images = document.getElementsByTagName('img');
-                for (let i = 0; i < images.length; i++) {
-                    if (images[i].className != 'match') {
-                        images[i].setAttribute('src', 'images/card-back.png');
+
+                document.getElementById('scores').innerHTML = '<h4>No match was made...</h4>';
+
+                setTimeout(function()   {
+                    for (let i = 0; i < images.length; i++) {
+                        if (images[i].className != 'match') {
+                            images[i].setAttribute('src', 'images/card-back.png');
+                        }
                     }
-                }
+                }, 2000);
                 match.cards_picked = 0;
                 if  (match.curr_player == 0)  {
                     match.curr_player = 1;
@@ -169,7 +191,10 @@
                     match.curr_player = 0;
                 }
             }
-            setupGame();
+            console.log(match.flipped);
+            setTimeout(function()   {
+                setupGame();
+            }, 2000);
         }
     }
 }());
