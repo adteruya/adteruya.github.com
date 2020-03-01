d1482c22c0e611d1cfe0682660055a376efb1e62
Ana Teruya <adteruya@ucdavis.edu>
2020-03-01 15:47:10 -0800

    styled default page

--- des157-wi20/studio4/script.js
+++ des157-wi20/studio4/script.js
@@ -3,6 +3,6 @@
     const button_nav = document.querySelector('div.before');
 
     if (button_nav.className == "before")   {
-        button_nav.innerHTML = '<div><button id="rules">Rules</button><button id="start">Start Game</button><button id="options">Options</button></div>';
+        button_nav.innerHTML = '<button id="rules">Rules</button><button id="start">Start Game</button><button id="options">Options</button>';
     }
 }());
