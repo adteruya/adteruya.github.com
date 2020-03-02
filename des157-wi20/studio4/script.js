888abad40b8704d18a793ef06e3e9b872db1af11
Ana Teruya <adteruya@ucdavis.edu>
2020-03-02 12:41:38 -0800

    Option button works

--- des157-wi20/studio4/script.js
+++ des157-wi20/studio4/script.js
@@ -1,9 +1,19 @@
 (function() {
     "use strict";
-    const theme = document.querySelector('body');
     const button_nav = document.getElementById('button-nav');
     const box = document.getElementById('pop-up');
     let curr_state;
+
+    // Game Data:
+    let match = {
+        players: ['Player 1', 'Player 2'],
+        score: [0, 0],
+        curr_player: 0,
+        cards_picked: 0,
+        flipped_cards: 0,
+        theme_color: 'red',
+        end_game_condition: 6
+    };
 
     // Displays buttons BEFORE game starts:
     if (button_nav.className == "before")   {
@@ -47,12 +57,203 @@
         // Adding text:
         button_nav.setAttribute('class', 'invisible');
         box.setAttribute('class', 'option-text');
-        box.innerHTML = 
-        '<div><h3>Options</h3><div class="reverse"><h4>Number of Cards</h4><button>4</button><button>6</button><button>8</button><button>10</button></div><div class="reverse"><h4>Theme Color</h4><button>Red</button><button>Yellow</button><button>Blue</button></div><div id="exit-options" class="reverse"><button>Okay</button></div></div>';
+        box.innerHTML = '<div><h3>Options</h3><div class="reverse"><h4>Number of Cards</h4><div id="card-options"><button id="four" class="not-chosen">4</button><button id="six" class="chosen">6</button><button id="eight" class="not-chosen">8</button><button id="ten" class="not-chosen">10</button></div></div><div class="reverse"><h4>Theme Color</h4><div id="color-options"><button id="r" class="chosen">Red</button><button id="y" class="not-chosen">Yellow</button><button id="b" class="not-chosen">Blue</button></div></div><div id="exit-options" class="reverse"><button>Okay</button></div></div>';
 
         // Stylizing:
-        document.getElementById('exit-options').style.cssText = 'text-align: center; margin-left: auto; margin-right: auto;';
+        let card_btns = box.querySelectorAll('#card-options button');
+        let color_btns = box.querySelectorAll('#color-options button');
+
+        if (match.theme_color == 'red') {
+            for (let i = 0; i < card_btns.length; i++)  {
+                if (card_btns[i].className == 'chosen') {
+                    card_btns[i].style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+                }
+            }
+            for (let i = 0; i < color_btns.length; i++) {
+                if (color_btns[i].className == 'chosen')    {
+                    color_btns[i].style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+                }
+            }
+        } else if (match.theme_color == 'yellow')   {
+            for (let i = 0; i < card_btns.length; i++)  {
+                if (card_btns[i].className == 'chosen') {
+                    card_btns[i].style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+                }
+            }
+            for (let i = 0; i < color_btns.length; i++) {
+                if (color_btns[i].className == 'chosen')    {
+                    color_btns[i].style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+                }
+            }
+        } else  {
+            for (let i = 0; i < card_btns.length; i++)  {
+                if (card_btns[i].className == 'chosen') {
+                    card_btns[i].style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+                }
+            }
+            for (let i = 0; i < color_btns.length; i++) {
+                if (color_btns[i].className == 'chosen')    {
+                    color_btns[i].style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+                }
+            }
+        }
+
+        box.style.marginTop = '250px';
+        document.getElementById('exit-options').style.cssText = 'text-align: center; margin-left: auto; margin-right: auto; padding-bottom: 20px; padding-top: 10px;';
         box.querySelector('h3').style.textAlign = 'center';
+
+        let h4 = box.querySelectorAll('h4');
+        for (let i = 0; i < h4.length; i++) {
+            h4[i].style.textAlign = 'center';
+        }
+
+        // Click to change number of cards in game:
+        document.getElementById('four').addEventListener('click', function(e)   {
+            e.preventDefault();
+            for (let i = 0; i < card_btns.length; i++)  {
+                card_btns[i].className = 'not-chosen';
+                if (match.theme_color == 'red') {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #92140C; color: #92140C;';
+                } else if (match.theme_color == 'yellow')  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #E6AC00; color: #E6AC00;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #3366CC; color: #3366CC;';
+                }
+            }
+            match.end_game_condition = 4;
+            document.getElementById('four').className = 'chosen';
+            if (match.theme_color == 'red') {
+                document.getElementById('four').style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+            } else if (match.theme_color == 'yellow')   {
+                document.getElementById('four').style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+            } else  {
+                document.getElementById('four').style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+            }
+        });
+
+        document.getElementById('six').addEventListener('click', function(e)   {
+            e.preventDefault();
+            for (let i = 0; i < card_btns.length; i++)  {
+                card_btns[i].className = 'not-chosen';
+                if (match.theme_color == 'red') {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #92140C; color: #92140C;';
+                } else if (match.theme_color == 'yellow')  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #E6AC00; color: #E6AC00;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #3366CC; color: #3366CC;';
+                }
+            }
+            match.end_game_condition = 6;
+            document.getElementById('six').className = 'chosen';
+            if (match.theme_color == 'red') {
+                document.getElementById('six').style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+            } else if (match.theme_color == 'yellow')   {
+                document.getElementById('six').style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+            } else  {
+                document.getElementById('six').style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+            }
+        });
+
+        document.getElementById('eight').addEventListener('click', function(e)   {
+            e.preventDefault();
+            for (let i = 0; i < card_btns.length; i++)  {
+                card_btns[i].className = 'not-chosen';
+                if (match.theme_color == 'red') {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #92140C; color: #92140C;';
+                } else if (match.theme_color == 'yellow')  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #E6AC00; color: #E6AC00;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #3366CC; color: #3366CC;';
+                }
+            }
+            match.end_game_condition = 8;
+            document.getElementById('eight').className = 'chosen';
+            if (match.theme_color == 'red') {
+                document.getElementById('eight').style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+            } else if (match.theme_color == 'yellow')   {
+                document.getElementById('eight').style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+            } else  {
+                document.getElementById('eight').style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+            }
+        });
+
+        document.getElementById('ten').addEventListener('click', function(e)   {
+            e.preventDefault();
+            for (let i = 0; i < card_btns.length; i++)  {
+                card_btns[i].className = 'not-chosen';
+                if (match.theme_color == 'red') {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #92140C; color: #92140C;';
+                } else if (match.theme_color == 'yellow')  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #E6AC00; color: #E6AC00;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: transparent; border-color: #3366CC; color: #3366CC;';
+                }
+            }
+            match.end_game_condition = 10;
+            document.getElementById('ten').className = 'chosen';
+            if (match.theme_color == 'red') {
+                document.getElementById('ten').style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+            } else if (match.theme_color == 'yellow')   {
+                document.getElementById('ten').style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+            } else  {
+                document.getElementById('ten').style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+            }
+        });
+
+        // Click to change theme color:
+        document.getElementById('r').addEventListener('click', function(e)  {
+            e.preventDefault();
+            match.theme_color = 'red';
+            document.querySelector('body').className = 'red';
+            for (let i = 0; i < color_btns.length; i++)  {
+                color_btns[i].className = 'not-chosen';
+                color_btns[i].style.cssText = 'background-color: transparent; border-color: #92140C; color: #92140C;';
+            } for (let i = 0; i < card_btns.length; i++) {
+                if (card_btns[i].className == 'chosen') {
+                    card_btns[i].style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: #FFF8F0; border-color: #92140C; color: #92140C;';
+                }
+            }
+            document.getElementById('r').className = 'chosen';
+            document.getElementById('r').style.cssText = 'background-color: #92140C; border-color: #FFF8F0; color: #FFF8F0;';
+        });
+
+        document.getElementById('y').addEventListener('click', function(e)  {
+            e.preventDefault();
+            match.theme_color = 'yellow';
+            document.querySelector('body').className = 'yellow';
+            for (let i = 0; i < color_btns.length; i++)  {
+                color_btns[i].className = 'not-chosen';
+                color_btns[i].style.cssText = 'background-color: transparent; border-color: #E6AC00; color: #E6AC00;';
+            } for (let i = 0; i < card_btns.length; i++) {
+                if (card_btns[i].className == 'chosen') {
+                    card_btns[i].style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: #FFF8F0; border-color: #E6AC00; color: #E6AC00;';
+                }
+            }
+            document.getElementById('y').className = 'chosen';
+            document.getElementById('y').style.cssText = 'background-color: #E6AC00; border-color: #FFF8F0; color: #FFF8F0;';
+        });
+
+        document.getElementById('b').addEventListener('click', function(e)  {
+            e.preventDefault();
+            match.theme_color = 'blue';
+            document.querySelector('body').className = 'blue';
+            for (let i = 0; i < color_btns.length; i++)  {
+                color_btns[i].className = 'not-chosen';
+                color_btns[i].style.cssText = 'background-color: transparent; border-color: #3366CC; color: #3366CC;';
+            } for (let i = 0; i < card_btns.length; i++) {
+                if (card_btns[i].className == 'chosen') {
+                    card_btns[i].style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+                } else  {
+                    card_btns[i].style.cssText = 'background-color: #FFF8F0; border-color: #3366CC; color: #3366CC;';
+                }
+            }
+            document.getElementById('b').className = 'chosen';
+            document.getElementById('b').style.cssText = 'background-color: #3366CC; border-color: #FFF8F0; color: #FFF8F0;';
+        });
 
         // Click to exit options:
         document.getElementById('exit-options').addEventListener('click', function(e) {
